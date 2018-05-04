eb05e4c20923e0a9eb7b24566688f2b98a884bf3
Harris Gomez <harrisgomez28@gmail.com>
2018-05-03 22:46:18 -0400

    added media query for hexagon

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,26 +1,25 @@
 const About = () => {
     return (
         <div id='about-component'>
-            <button type="button" className="btn btn-secondary">Secondary</button>
             <div className="header">
                 <h1>ABOUT</h1>
                 <div className="">-</div>
             </div>
             <div className="skills">
-                <div className="hexagon">
-                    <div className="hexagon-inside">
-                        <div className="hexagon-me"></div>
+                <div className='profile'>
+                    <div className="hexagon">
+                        <div className="hexagon-inside">
+                            <div className="hexagon-me"></div>
+                        </div>
                     </div>
                 </div>
                 <div className='skill-levels'>
-                    <ul>
-                        <li id='skill-1'>CSS</li>
-                        <li id='skill-2'>HTML</li>
-                        <li id='skill-3'>JavaScript</li>
-                        <li id='skill-4'>Webpack</li>
-                        <li id='skill-5'>UI Design</li>
-                        <li id='skill-6'>Responsive Design</li>
-                    </ul>
+                    <div id='skill-1'>CSS</div>
+                    <div id='skill-2'>HTML</div>
+                    <div id='skill-3'>JavaScript</div>
+                    <div id='skill-4'>Webpack</div>
+                    <div id='skill-5'>UI Design</div>
+                    <div id='skill-6'>Responsive Design</div>
                 </div>
             </div>
             <div className="row">
