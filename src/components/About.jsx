928b9b23fc926956b4d9eb26cc53152569e5c890
Harris Gomez <harrisgomez28@gmail.com>
2018-05-06 03:14:24 -0400

    bar animation complete

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,28 +2,40 @@ const About = () => {
     return (
         <div id='about-component'>
             <div className="header">
-                <h1>ABOUT</h1>
-                <div className="">-</div>
+                <h1 className="header">ABOUT</h1>
             </div>
-            <div className="skills">
-                <div className='profile'>
-                    <div className="hexagon">
-                        <div className="hexagon-inside">
-                            <div className="hexagon-me"></div>
+            <div className='table'>
+                <div className="skills table-row">
+                    <div className='profile table-cell'>
+                        <div className="hexagon">
+                            <div className="hexagon-inside">
+                                <div className="hexagon-me"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='skill-levels table-cell'>
+                        <div className='table'>
+                            <div className='table-row'>
+                                <div className='skill-html table-cell bar'>HTML</div>
+                            </div>
+                            <div className='table-row'>
+                                <div className='skill-css table-cell bar'>CSS</div>
+                            </div>
+                            <div className='table-row'>
+                                <div className='skill-js table-cell bar'>JavaScript</div>
+                            </div>
+                            <div className='table-row'>
+                                <div className='skill-webpack table-cell bar'>Webpack</div>
+                            </div>
+                            <div className='table-row'>
+                                <div className='skill-ui table-cell bar'>UI Design</div>
+                            </div>
+                            <div className='table-row'>
+                                <div className='skill-responsive table-cell bar'>Responsive Design</div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className='skill-levels'>
-                    <div id='skill-1'>CSS</div>
-                    <div id='skill-2'>HTML</div>
-                    <div id='skill-3'>JavaScript</div>
-                    <div id='skill-4'>Webpack</div>
-                    <div id='skill-5'>UI Design</div>
-                    <div id='skill-6'>Responsive Design</div>
-                </div>
-            </div>
-            <div className="row">
-
             </div>
         </div>
     );
